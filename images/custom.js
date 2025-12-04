4e17d910acb257fb6ac07fff822e247994a1d324
Andrew Shin <andrew.shin.999@outlook.com>
2025-12-03 20:38:20 -0800

    Update custom.js

--- images/custom.js
+++ images/custom.js
@@ -1,12 +1,9 @@
-// /images/custom.js
-// Force custom page title
 document.title = "testweb";
 
-// Force custom favicon
-var link = document.querySelector('link[rel="shortcut icon"]');
+var link = document.querySelector("link[rel='shortcut icon']");
 if (!link) {
-    link = document.createElement('link');
-    link.rel = 'shortcut icon';
+    link = document.createElement("link");
+    link.rel = "shortcut icon";
     document.head.appendChild(link);
 }
-link.href = "images/favicon.png"; // Make sure this matches the path in the container
+link.href = "images/favicon.png";
